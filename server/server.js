bd152fa29f79c1ea3cb96626d4ca69e27def3714
lcchai <lc.chai.sg@gmail.com>
2023-10-12 17:54:00 +0800

    implement socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
+const http = require("http");
 const path = require("path");
 const express = require("express");
 require("dotenv").config();
+const socketIO = require("socket.io");
 const PORT = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, "/../public");
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+    console.log("A new user just connected!");
+});
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 });
